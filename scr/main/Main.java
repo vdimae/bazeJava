2f86a41bcb39e8679177c674717e0dbba97f4f62
vdimae <vdimae@mail.ru>
2018-10-14 21:00:51 +0300

    добавлен пакет main, плюс в нем создан класс Main

---
+++ scr/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Create by vdimae on 14.10.18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
